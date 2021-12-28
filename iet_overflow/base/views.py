9d1f24c96786c3b8ae38f1d8724438ce8ce763a6
mario11-wiet <mk.11.03@wp.pl>
2021-12-28 07:09:15 +0100

    Search and POST,GET methods

--- iet_overflow/base/views.py
+++ iet_overflow/base/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
+from .forms import RoomForm
+from django.db.models import Q
 
 
 # Create your views here.
@@ -9,8 +11,13 @@ def okay(request):
 
 
 def home(response):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = response.GET.get('q') if response.GET.get('q') is not None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms': rooms,
+               'topics': topics,
+               'room_count': room_count}
     return render(response, 'base/home.html', context)
 
 
@@ -22,5 +29,34 @@ def room(response, id):
 
 
 def createRoom(response):
-    context = {}
-    return render(response,'base/room_form.html', context)
+    form = RoomForm()
+    if response.method == 'POST':
+        form = RoomForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(response, 'base/room_form.html', context)
+
+
+def updateRoom(response, id):
+    room = Room.objects.get(id=id)
+    form = RoomForm(instance=room)
+
+    if response.method == "POST":
+        form = RoomForm(response.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(response, 'base/room_form.html', context)
+
+
+def deleteRoom(response, id):
+    room = Room.objects.get(id=id)
+    if response.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(response, 'base/delete.html', {'obj': room})
